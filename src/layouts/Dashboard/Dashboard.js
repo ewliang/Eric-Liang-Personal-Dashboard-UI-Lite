f521f2d57e674ff9cc07a3e49a6f9f1021919516
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-11-22 10:53:57 -0800

    Initial commit.

---
+++ src/layouts/Dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Dashboard.scss';
+
+// Components
+import MainNav from '../../components/MainNav/MainNav';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className = "main-dashboard">
+        <MainNav></MainNav>
+        <main>
+          { this.props.children }
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
