bcdd7ed3bec1b19bde89e231d8de2112935fd31a
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-11-22 16:07:29 -0800

    Removed default CRA generated code and replaced with some code for dashboard.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import Dashboard from './layouts/Dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <Dashboard>
+        <header>
+          <h1>Home</h1>
         </header>
-      </div>
+        <div>
+          <div className = "text-center">
+            <div className = "activeVisitorsValue">30</div>
+            <div className = "activeVisitorsLabel">Active Visitors</div>
+          </div>
+          <form>
+            <label>Direct</label>&nbsp;&nbsp;<progress value="22" max="100"></progress>
+            <label>Direct</label>&nbsp;&nbsp;<progress value="22" max="100"></progress>
+            <label>Direct</label>&nbsp;&nbsp;<progress value="22" max="100"></progress>
+          </form>
+        </div>
+      </Dashboard>
     );
   }
 }
