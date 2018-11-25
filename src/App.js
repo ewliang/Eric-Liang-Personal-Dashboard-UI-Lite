315f5f7aee3305a2b2fdd159164ad223012dc402
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-11-24 18:10:23 -0800

    Chart.js dependency added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Chart from 'chart.js';
 
 import Dashboard from './layouts/Dashboard/Dashboard';
 
 class App extends Component {
+  componentDidMount() {
+    var visitorsChartContext = document.getElementById('visitorsChart').getContext('2d');
+    var chart = new Chart(visitorsChartContext, {
+      type: 'line',
+      data: {
+        labels: [0, 1, 2, 3],
+        datasets: [{
+          label: 'My Dataset',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [5, 1, 3, 0]
+        }]
+      },
+      options: {}
+    });
+  }
+
   render() {
     return (
       <Dashboard>
@@ -15,12 +33,29 @@ class App extends Component {
             <div className = "activeVisitorsValue">30</div>
             <div className = "activeVisitorsLabel">Active Visitors</div>
           </div>
-          <form>
-            <label>Direct</label>&nbsp;&nbsp;<progress value="22" max="100"></progress>
-            <label>Direct</label>&nbsp;&nbsp;<progress value="22" max="100"></progress>
-            <label>Direct</label>&nbsp;&nbsp;<progress value="22" max="100"></progress>
-          </form>
+          <br/>
+          <table>
+            <tr><td>Desktop</td><td><progress value="50" max="100"></progress></td></tr>
+            <tr><td>Mobile</td><td><progress value="30" max="100"></progress></td></tr>
+            <tr><td>Tablet</td><td><progress value="20" max="100"></progress></td></tr>
+          </table>
         </div>
+        <canvas id = "visitorsChart">
+        </canvas>
+        <table>
+          <tr>
+            <td>ID</td>
+            <td>URL</td>
+            <td>Views</td>
+            <td>Bounce Rate</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
       </Dashboard>
     );
   }
